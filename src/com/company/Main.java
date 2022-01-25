7db2689b9aeaafa266aa46d3db8184fcfef2244c
Dan Wright <wrightdj99@gmail.com>
2022-01-25 16:55:07 -0600

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,15 @@
 package com.company;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+
+    //First, I need to define the parameters for the driver name and URL; the H2 Driver
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
 
     public static void main(String[] args) {
 
